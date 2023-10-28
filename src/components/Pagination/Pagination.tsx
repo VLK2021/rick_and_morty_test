36e6db3a3d5501d20876e1a4c035ca6b7d3b7774
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-10-28 03:20:48 +0300

    finished pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -8,7 +8,7 @@ import {getAllCharacters} from "../../store/slices/character.slice";
 
 // @ts-ignore
 const Pagination = ({count}) => {
-    const endPagesFinal = Math.ceil(count / 138);
+    const endPagesFinal = Math.ceil(count / 82.6);
 
     const dispatch = useDispatch();
 
@@ -29,8 +29,32 @@ const Pagination = ({count}) => {
     }
 
     const onPageChange = (page: number) => {
+        //найменша та найбільша сторінки. Заборона побудови сторінок
+        if (page < 1) {
+            return;
+        }
+        if (page === 42) {
+            return;
+        }
+
+        //побудова останнього блоку сторінок
+        if (page > 40) {
+            setStartPage(page);
+            setEndPage(page +1);
+        }
+
+        //динамічна зміна сторінок
+        if (page > endPage && page < 33) {
+            setStartPage(page);
+            setEndPage(page + 9);
+        }
+        if (page < startPage && page > 1) {
+            setEndPage(page);
+            setStartPage(page - 9);
+        }
+
         dispatch(getAllCharacters({page}));
-            setPage(page);
+        setPage(page);
     };
 
     return (
@@ -48,7 +72,7 @@ const Pagination = ({count}) => {
             {
                 pages.map(item =>
                     <div key={item}
-                         className={`pagination-pages ${page === item && 'active-page'}`}
+                         className={`pagination-pages flex ${page === item && 'active-page'}`}
                          onClick={() => {
                              onPageChange(item);
                          }}>{item}
